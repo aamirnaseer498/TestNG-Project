8db3dc35e9256133d803e6ecc92103009d0f00c1
Aamir Naseer <aamirnaseer498@gmail.com>
2023-10-11 12:02:00 +0500

    Committed introduction

---
+++ src/test/java/FirstTestCase.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class FirstTestCase {
+
+    @Test (priority = 1)
+    public void setup(){
+        System.out.println("Opening browser");
+    }
+
+    @Test (priority = 2)
+    public void login(){
+        System.out.println("This is login test");
+    }
+
+    @Test (priority = 3)
+    public void teardown(){
+        System.out.println("Closing browser");
+    }
+
+}
